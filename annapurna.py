d39bf8cc0eba9a859bd513f0114f30cd56fadb7e
Anup Timsina <git.timsinaanup@gmail.com>
2022-12-13 20:50:26 +0545

    first commit

---
+++ annapurna.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+info = []
+
+try:
+    for n in range(1,6):
+        url = "https://bg.annapurnapost.com/api/tags/news?page="+ str(n) +"&per_page=10&tag=%E0%A4%B8%E0%A4%82%E0%A4%B8%E0%A4%A6%E0%A5%80%E0%A4%AF-%E0%A4%A8%E0%A4%BF%E0%A4%B0%E0%A5%8D%E0%A4%B5%E0%A4%BE%E0%A4%9A%E0%A4%A8"
+        payload={}
+        headers = {}
+
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        jsondata= json.loads(response.text)
+        articles = jsondata['data']
+        for article in articles:
+            author = article['author']['name']
+            news = article['title']
+            date = article['publishOn']
+            info.append({
+            'author': author,
+            'news' : news,
+            'date' : date
+            })
+
+except:
+    print("Error occured")
+
+print(info)
